966491123f936071e6a402ce00df98c0788bcb5e
chennisden <proofprogram@gmail.com>
2020-08-08 21:02:55 -0700

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 document.write("<div class=\"main\">");
 document.write("<div class=\"topnav\">");
-document.write("<a href=\"https://www.geometryexplorer.xyz\">Exploring Euclidean Geometry<\/a>");
+document.write("<a href=\"https:\/\/www.geometryexplorer.xyz\">Exploring Euclidean Geometry<\/a>");
 document.write("<a href=\"\/texfiles\/CV\/cv.pdf\">CV<\/a>");
 document.write("<div class=\"topnav-right\">");
 document.write("<a href=\"\/essay\">Essays</a>");
