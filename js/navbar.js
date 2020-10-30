5556aa6a70b27c8e3d1abe686aa954f2b22131ca
chennisden <proofprogram@gmail.com>
2020-10-29 22:07:11 -0700

    sadpsadsadsa

--- js/navbar.js
+++ js/navbar.js
@@ -2,9 +2,11 @@ document.write("<div class=\"main\">");
 document.write("<div class=\"topnav\">");
 document.write("<a href=\"https:\/\/www.geometryexplorer.xyz\">Home<\/a>");
 document.write("<a href=\"\/texfiles\/CV\/cv.pdf\">CV<\/a>");
+document.write("<a href=\"https:\/\/mac.geometryexplorer.xyz\/\">MAC<\/a>");
 document.write("<div class=\"topnav-right\">");
 document.write("<a href=\"\/essay\">Essays</a>");
 document.write("<a href=\"\/handouts\">Handouts<\/a>");
 document.write("<a href=\"https:\/\/mast.geometryexplorer.xyz\/\">MAST<\/a>");
+
 document.write("<\/div>");
 document.write("<\/div>");
